fd8390f67af3bfd9c22e897938d1a7d5401480e2
Hagmann Stephan <stephanhagmann@outlook.com>
2025-03-18 13:47:29 +0000

    Added attributions

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ import resources.routers as routers
  
 app = FastAPI(
     title="Movie Finder - Backend",
-    description="MovieFinder Backend with FastAPi and PostgreSQL",
+    description="""
+MovieFinder Backend with FastAPi and PostgreSQL.
+
+## Attribution
+
+<a href="https://www.themoviedb.org/" target="_"><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB Logo" width="80px" style="margin: 10px 0 15px 0;"></a>
+**This app uses [TMDB](https://www.themoviedb.org/) and the [TMDB APIs](https://developer.themoviedb.org/docs/getting-started) but is not endorsed, certified, or otherwise approved by TMDB.**
+    """,
     version="0.1.0"
 )
 
